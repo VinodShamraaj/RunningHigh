4402d2c81762cb08d2753bdbd6109f266122bdac
VinodShamraaj <vinodshamraaj@gmail.com>
2024-05-15 00:12:13 +0800

    feat: Added Player Idle and Running animations

--- Assets/Scripts/GameControllers/PlayerController.cs
+++ Assets/Scripts/GameControllers/PlayerController.cs
@@ -1,25 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class PlayerController : MonoBehaviour
 {
     public JoystickController joystickController;
     public float playerSpeed;
+    private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidBody;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (joystickController.joystickVector.x != 0)
+        float joystickX = joystickController.joystickVector.x;
+
+        // Handle Animations
+        spriteRenderer.flipX = joystickX < 0f;
+        animator.SetFloat("Velocity", Math.Abs(joystickX));
+
+
+
+        if (joystickX != 0)
         {
-            rigidBody.velocity = new Vector2(joystickController.joystickVector.x * playerSpeed, 0);
+            rigidBody.velocity = new Vector2(joystickX * playerSpeed, 0);
         }
         else
         {
